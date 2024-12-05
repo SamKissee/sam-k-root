55e439d273272ce4bcf933b1ccbc15fdea0e1cfe
Sam Kissee <samkissee@gmail.com>
2024-12-04 19:24:51 -0700

    add random generator and comment

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -5,13 +5,26 @@ import FilterBar from "@/components/FilterBar";
 import useNotificationStore from "@/stores/useNotificationStore";
 import sortNotifications from "@/utils/sortNotifications";
 import NotificationItem from "@/components/NotificationItem";
-import { Notification } from "@/stores/useNotificationStore";
+import generateNotification from "@/utils/generateNotification";
 
 export default function Notifications() {
-  const { notifications } = useNotificationStore();
+  const { notifications, addNotification } = useNotificationStore();
   const [filter, setFilter] = React.useState<string | null>(null);
 
+  React.useEffect(() => {
+    // runs randomly between 30s and 2min
+    const randomTimeout = () => Math.random() * (120000 - 30000) + 30000;
+    // kind of a code blob, but sets a new timeout at the end of the current timeout.
+    let timeout = setTimeout(function run() {
+      addNotification(generateNotification());
+      timeout = setTimeout(run, randomTimeout());
+    }, randomTimeout());
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   const data = React.useMemo(() => {
+    // simple sort/filter memo. If All selected the filter is null.
     if (!filter) {
       return sortNotifications(notifications);
     } else {
